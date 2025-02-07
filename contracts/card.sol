a4b2405a352a0f0dd5b98492a23ee78c620eb78c
henriquelalvim <henriquelalvim@gmail.com>
2025-02-07 17:53:38 -0300

    incluindo mais informações na carta de monstro

--- contracts/card.sol
+++ contracts/card.sol
@@ -8,19 +8,46 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 contract Card is ERC1155, Ownable, ERC1155Burnable {
     constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
 
+    mapping(uint256 idMonsterCard => MonsterAttributes) monsterAttributesMap;
+
     enum typeOfCards {
         monster,
         magic,
         trap
     }
+    
+    enum magicTypes {
+        increaseAttack,
+        increaseDefense,
+        increaseLife,
+        increaseMana
+    }
 
-    struct monsterAttributes {
+    struct MonsterAttributes {
         uint256 attack;
         uint256 defense;
+        uint256 life;
+        uint256 mana;
     }
 
     struct trapAtributes {
         bool revealed;
+    }
+
+    
+
+    
+    function createMonsterCard(MonsterAttributes memory monsterAttributes, uint256 id) public onlyOwner {
+        require(id % 3 == 0, "Not a valid monster card id");
+        monsterAttributesMap[id] = monsterAttributes;    
+    }
+
+    function createMagicCard() external {
+
+    }
+
+    function createTrapCard() external {
+
     }
 
     function setURI(string memory newuri) public onlyOwner {
